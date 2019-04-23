8a577e58064ac82e3b19e9e01b9e40bbd44ada02
Martin Kruse <mkruse@prodyna.com>
2019-04-23 09:36:22 +0200

    fix build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   serviceWorkerEnabled: true,
-  apiBaseUrl: 'http://localhost:3001' // TODO
+  apiBaseUrl: 'http://localhost:3001', // TODO
+  oAuthRedirectURL: 'https://d2y3erpolekeff.cloudfront.net/loggedin',
+  clientId: 'amzn1.application-oa2-client.f71167f853b847ac952643b17c7df850',
+  scope: 'profile email',
 };
