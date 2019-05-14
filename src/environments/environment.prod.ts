ba3ae943caea17a48752b1ae7ee63e37624e786e
Martin Kruse <mkruse@prodyna.com>
2019-05-14 23:48:12 +0200

    PAC-27 connect api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   serviceWorkerEnabled: true,
-  apiBaseUrl: 'http://localhost:3001', // TODO
+  apiBaseUrl: 'https://aij4t1he5a.execute-api.eu-central-1.amazonaws.com/test',
   oAuthRedirectURL: 'https://d2y3erpolekeff.cloudfront.net/loggedin',
   clientId: 'amzn1.application-oa2-client.f71167f853b847ac952643b17c7df850',
   scope: 'profile email',
