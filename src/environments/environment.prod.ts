b939eff35f528fdc55646497c8d3323c08f82050
Martin Kruse <mkruse@prodyna.com>
2019-05-26 22:39:30 +0200

    fix topics and env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   serviceWorkerEnabled: true,
   apiBaseUrl: 'https://aij4t1he5a.execute-api.eu-central-1.amazonaws.com/test',
-  oAuthRedirectURL: 'http://localhost:8080/loggedin',
+  oAuthRedirectURL: 'https://d2y3erpolekeff.cloudfront.net/loggedin',
   clientId: 'amzn1.application-oa2-client.f71167f853b847ac952643b17c7df850',
   scope: 'profile email',
 };
